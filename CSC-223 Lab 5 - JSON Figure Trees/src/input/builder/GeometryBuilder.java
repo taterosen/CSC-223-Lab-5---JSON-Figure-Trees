94adacec5aafc9499558240a477be5b337eb42c3
taterosen <taterosen@gmail.com>
2022-10-12 14:41:45 -0400

    initial changes to GeoBuilder and Parser
    methods in GB and parser beginning to be updated

---
+++ CSC-223 Lab 5 - JSON Figure Trees/src/input/builder/GeometryBuilder.java
@@ -0,0 +1,51 @@
+package input.builder;
+
+import java.util.List;
+
+/**
+ * 
+ * @author taterosen
+ * @date 10/11/2022
+ */
+
+
+public class GeometryBuilder extends DefaultBuilder {
+
+	@Override
+	public FigureNode buildFigureNode(String description,
+										PointNodeDatabase points,
+										SegmentNodeDatabase segments)
+	{
+		return new FigureNode(description, points, segments);
+	}
+	
+	@Override
+	public SegmentNodeDatabase buildSegmentNodeDatabase()
+    {
+        return new SegmentNodeDatabase();
+    }
+	
+	public void addSegmentToDatabase(SegmentNodeDatabase segments, PointNode from, PointNode to)
+    {
+    	super(segments, from, to);
+    }
+	
+	@Override
+	public SegmentNode buildSegmentNode(PointNode pt1, PointNode pt2)
+    {
+        return new SegmentNode(pt1, pt2);
+    }
+
+	@Override
+	public PointNodeDatabase buildPointDatabaseNode(List<PointNode> points)
+    {
+        return new PointNodeDatabase(points);
+    }
+    
+	@Override
+    public PointNode buildPointNode(String name, double x, double y)
+    {
+        return new PointNode(name, x, y);
+    }
+
+}
---
+++ CSC-223 Lab 5 - JSON Figure Trees/src/input/parser/JSONParser.java
@@ -0,0 +1,173 @@
+/**
+ * Parses from a JSON file to a tree and unparses in reverse
+ * JSON file looks like:
+ * { "Triangle" :
+ *   { "Description" : "triangle with three points",
+ *     "Points" : [ { "name" : "A", "x" : 0, "y" : 0 }
+ *                  { "name" : "B", "x" : 1, "y" : 1 }
+ *                  { "name" : "C", "x" : 0, "y" : 1 }
+ *                ]
+ *     "Segments" : [ { "A" : ["B", "C"] }
+ *                    { "B" : ["C", "A"] }
+ *                    { "C" : ["B", "A"] }
+ *                  ]
+ *   }
+ * }
+ *  To a tree structure like:
+ *               |-- Description
+ *               |
+ *  FigureNode --|-- PointNodeDatabase (contains a Set of PointNode, each containing a name, x, and y)
+ *               |
+ *               |-- SegmentNodeDatabase (contains a map of PointNode and PointNodeDatabase)
+ *  
+ *  @author Tate Rosen, Regan Richardson, Hanna King
+ *  @date 10/5/2022
+ */
+ 
+ package input.parser;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import input.components.*;
+import input.components.point.PointNode;
+import input.components.point.PointNodeDatabase;
+import input.components.segment.SegmentNodeDatabase;
+import input.exception.ParseException;
+
+public class JSONParser
+{
+	protected ComponentNode  _astRoot;
+
+	/**
+	 * constructor. sets _astRoot to null
+	 */
+	public JSONParser()
+	{
+		_astRoot = null;
+	}
+
+	/**
+	 * throws ParseException with message passed in
+	 * @param message explanation of error
+	 * @throws ParseException
+	 */
+	private void error(String message) throws ParseException
+	{
+		throw new ParseException("Parse error: " + message);
+	}
+
+	/**
+	 * Parse a string into a tree
+	 * @param str String to parse
+	 * @return ComponentNode, the root of the tree built from str
+	 * @throws ParseException
+	 */
+	public ComponentNode parse(String str) throws ParseException
+	{
+		// Parsing is accomplished via the JSONTokenizer class.
+		JSONTokener tokenizer = new JSONTokener(str);
+		JSONObject  JSONroot = (JSONObject)tokenizer.nextValue();
+		
+		try
+			{
+				JSONroot.getJSONObject(JSON_Constants.JSON_FIGURE);
+			}
+				
+		catch (JSONException e)
+			{
+				throw new ParseException(e);
+			}
+
+		JSONObject fig = JSONroot.getJSONObject(JSON_Constants.JSON_FIGURE);
+		
+		DefaultBuilder builder = new DefaultBuilder():
+
+		String description = parseDescription(fig.getString(JSON_Constants.JSON_DESCRIPTION), builder);
+		PointNodeDatabase points = parsePoints(fig.getJSONArray(JSON_Constants.JSON_POINT_S), builder);
+		
+		SegmentNodeDatabase segments = parseSegments(fig.getJSONArray(JSON_Constants.JSON_SEGMENTS), points, builder);
+		
+		 
+		
+		return new FigureNode(description, points, segments);
+	}
+	
+	/**
+	 * Get the description of the figure as a String
+	 * @param figure JSONObject for the figure
+	 * @return String value for the "Description" key
+	 */
+	private String parseDescription(String description, DefaultBuilder builder)
+	{
+		
+		return description;
+	}
+	
+	/**
+	 * Convert JSONObject points to PointNodes and add to PointNodeDatabase.
+	 * @param arr JSONArray of points
+	 * @return PointNodeDatabase of PointNode objects for the figure
+	 */
+	private PointNodeDatabase parsePoints(JSONArray arr, DefaultBuilder builder)
+	{
+		PointNodeDatabase points = builder.buildPointNodeDatabase();
+		
+		for(int i = 0; i < arr.length(); i++)
+		{
+			JSONObject JSONpoint = arr.getJSONObject(i);
+			PointNode point = new PointNode(JSONpoint.getString(JSON_Constants.JSON_NAME), JSONpoint.getInt(JSON_Constants.JSON_X),
+											JSONpoint.getInt(JSON_Constants.JSON_Y));
+			points.put(point);
+		}
+		
+		return points;
+	}
+	
+	/**
+	 * Parses a JSONArray of segments in a JSON file and 
+	 * puts them into a SegmentNodeDatabase. 
+	 * @param arr JSONArray of JSONObject segments
+	 * @param points PointNodeDataBase of PointNode s
+	 * @return SegmentNodeDatabase for the figure
+	 */
+	private SegmentNodeDatabase parseSegments(JSONArray arr, PointNodeDatabase points, DefaultBuilder builder)
+	{
+		SegmentNodeDatabase snd = builder.buildSegmentNodeDatabase();
+		
+		for(int i = 0; i < arr.length(); i++)
+		{
+			JSONObject JSONseg = arr.getJSONObject(i);
+			
+			Iterator<String> segKeys = JSONseg.keys();
+			String currentKey = segKeys.next();
+
+			PointNode edgeStart = points.getPoint(currentKey);
+			
+			JSONArray JSONedgeEnds = JSONseg.getJSONArray(currentKey);
+			List<PointNode> edgeEnds = parseSegmentsHelper(JSONedgeEnds, points);
+			
+			snd.addAdjacencyList(edgeStart, edgeEnds);
+		}
+		
+		return snd;
+	}
+	
+	private List<PointNode> parseSegmentsHelper(JSONArray arr, PointNodeDatabase points)
+	{
+		List<PointNode> edgeEnds = new ArrayList<>();
+		for(int i = 0; i < arr.length(); i++)
+		{
+			edgeEnds.add(points.getPoint(arr.getString(i)));
+		}
+		
+		return edgeEnds;
+	}
+}
