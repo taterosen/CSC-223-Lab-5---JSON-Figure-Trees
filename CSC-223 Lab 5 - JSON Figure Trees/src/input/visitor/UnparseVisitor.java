5f0cd8a4b5293317e50e00209aa1a7c648604888
reganrichardson <92763866+reganrichardson@users.noreply.github.com>
2022-10-12 22:28:41 -0400

    FIXED visitFigureNode, visitPointNodeDatabase

--- CSC-223 Lab 5 - JSON Figure Trees/src/input/visitor/UnparseVisitor.java
+++ CSC-223 Lab 5 - JSON Figure Trees/src/input/visitor/UnparseVisitor.java
@@ -28,20 +28,22 @@ public class UnparseVisitor implements ComponentNodeVisitor
 		StringBuilder sb = pair.getKey();
 		int level = pair.getValue();
 		
+		//create an indent for figure
 		sb.append(StringUtilities.indent(level) + "Figure \n");
 
 		sb.append(StringUtilities.indent(level) + "{\n");
 
+		//create a new object that takes string builder object that is 1 level in
 		AbstractMap.SimpleEntry<StringBuilder, Integer> newPair = new AbstractMap.SimpleEntry<>(sb, level +1);
 		
         // unparse description
 		sb.append(StringUtilities.indent(level +1));
 		sb.append("Description : \"" + node.getDescription() + "\"");
 		
-		//unparse points
+		//calls visitPointsDatabase
 		node.getPointsDatabase().accept(this, newPair);
 		
-		//unparse segments
+		//calls visitSegments
 		node.getSegments().accept(this, newPair);
 		
 		sb.append(StringUtilities.indent(level) + "}\n");
@@ -61,6 +63,7 @@ public class UnparseVisitor implements ComponentNodeVisitor
 				
 		sb.append(StringUtilities.indent(level) + "Segments:\n" + StringUtilities.indent(level) + "{\n");
 		
+		//for each point node in our list indent 1 level in and print each point
 		for(PointNode p : node.getAdjList().keySet()) {
 			
 				sb.append(StringUtilities.indent(level + 1) + p.getName() + " : ");
@@ -98,8 +101,10 @@ public class UnparseVisitor implements ComponentNodeVisitor
 		
 		sb.append(StringUtilities.indent(level) + "Points:\n" + StringUtilities.indent(level) +  "{\n");
 		
+		AbstractMap.SimpleEntry<StringBuilder, Integer> newPair = new AbstractMap.SimpleEntry<>(sb, level +1);
+		
 		for(PointNode p : node.getPoints()) {
-			p.unparse(sb, level + 1);
+			p.accept(this, newPair);
 		}
 		
 		sb.append(StringUtilities.indent(level) + "}\n");
