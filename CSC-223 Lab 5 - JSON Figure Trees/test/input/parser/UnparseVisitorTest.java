e7ec6fa39aab7a54d47397a9c233378ce9b4916f
reganrichardson <92763866+reganrichardson@users.noreply.github.com>
2022-10-16 14:38:26 -0400

    UnparseVisitorTest FINISHED and PASSES

--- CSC-223 Lab 5 - JSON Figure Trees/test/input/parser/UnparseVisitorTest.java
+++ CSC-223 Lab 5 - JSON Figure Trees/test/input/parser/UnparseVisitorTest.java
@@ -178,5 +178,123 @@ class UnparseVisitorTest {
 		
 		assertEquals(expected, sb.toString());
 	}
+	
+	@Test
+	void fully_connected_irregular_polygonTest() {
+		
+		JSONParser Parse = new JSONParser();
+		String figureStr = utilities.io.FileUtilities.readFileFilterComments("jsonfiles/fully_connected_irregular_polygon.json");
+		ComponentNode node = Parse.parse(figureStr);
+		
+		StringBuilder sb = new StringBuilder();
+		UnparseVisitor unparser = new UnparseVisitor();
+		unparser.visitFigureNode((FigureNode)node, new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0));
+		
+		String expected = "Figure \n"
+				+ "{\n"
+				+ "    Description : \"Irregular pentagon in which each vertex is connected to each other.\"\n"
+				+ "    Points:\n"
+				+ "    {\n"
+				+ "        Point(A)(0.0, 0.0)\n"
+				+ "        Point(B)(4.0, 0.0)\n"
+				+ "        Point(C)(6.0, 3.0)\n"
+				+ "        Point(D)(3.0, 7.0)\n"
+				+ "        Point(E)(-2.0, 4.0)\n"
+				+ "        Point(F)(26.0, 0.0)\n"
+				+ "    }\n"
+				+ "    Segments:\n"
+				+ "    {\n"
+				+ "        E : A  B  C  D  \n"
+				+ "        C : A  B  E  D  \n"
+				+ "        B : A  C  E  D  \n"
+				+ "        A : B  C  E  D  \n"
+				+ "        D : A  B  C  E  \n"
+				+ "    }\n"
+				+ "}\n";
+		
+		assertEquals(expected, sb.toString());
+	}
+	
+	@Test
+	void snakeTest() {
+		
+		JSONParser Parse = new JSONParser();
+		String figureStr = utilities.io.FileUtilities.readFileFilterComments("jsonfiles/snake.json");
+		ComponentNode node = Parse.parse(figureStr);
+		
+		StringBuilder sb = new StringBuilder();
+		UnparseVisitor unparser = new UnparseVisitor();
+		unparser.visitFigureNode((FigureNode)node, new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0));
+		
+		String expected = "Figure \n"
+				+ "{\n"
+				+ "    Description : \"Three triangles glued by vertex in a row\"\n"
+				+ "    Points:\n"
+				+ "    {\n"
+				+ "        Point(A)(0.0, 0.0)\n"
+				+ "        Point(B)(0.0, 1.0)\n"
+				+ "        Point(C)(1.0, 0.0)\n"
+				+ "        Point(D)(2.0, 0.0)\n"
+				+ "        Point(E)(2.0, 1.0)\n"
+				+ "        Point(F)(3.0, 1.0)\n"
+				+ "        Point(G)(3.0, 0.0)\n"
+				+ "    }\n"
+				+ "    Segments:\n"
+				+ "    {\n"
+				+ "        G : F  E  \n"
+				+ "        F : G  E  \n"
+				+ "        E : F  G  C  D  \n"
+				+ "        D : C  E  \n"
+				+ "        C : A  B  D  E  \n"
+				+ "        B : A  C  \n"
+				+ "        A : B  C  \n"
+				+ "    }\n"
+				+ "}\n";
+		
+		assertEquals(expected, sb.toString());
+	}
+	
+	@Test
+	void Tri_QuadTest() {
+		
+		JSONParser Parse = new JSONParser();
+		String figureStr = utilities.io.FileUtilities.readFileFilterComments("jsonfiles/Tri_Quad.json");
+		ComponentNode node = Parse.parse(figureStr);
+		
+		StringBuilder sb = new StringBuilder();
+		UnparseVisitor unparser = new UnparseVisitor();
+		unparser.visitFigureNode((FigureNode)node, new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0));
+		
+		String expected = "Figure \n"
+				+ "{\n"
+				+ "    Description : \"Tri Quad Shape.\"\n"
+				+ "    Points:\n"
+				+ "    {\n"
+				+ "        Point(A)(4.0, 0.0)\n"
+				+ "        Point(B)(8.0, 0.0)\n"
+				+ "        Point(C)(4.0, 5.0)\n"
+				+ "        Point(D)(8.0, 5.0)\n"
+				+ "        Point(E)(0.0, 10.0)\n"
+				+ "        Point(F)(12.0, 10.0)\n"
+				+ "        Point(G)(4.0, 12.0)\n"
+				+ "        Point(H)(8.0, 12.0)\n"
+				+ "        Point(I)(6.0, 10.0)\n"
+				+ "    }\n"
+				+ "    Segments:\n"
+				+ "    {\n"
+				+ "        H : F  I  \n"
+				+ "        G : I  E  \n"
+				+ "        F : H  D  \n"
+				+ "        I : G  H  C  D  \n"
+				+ "        B : A  D  \n"
+				+ "        A : B  C  \n"
+				+ "        E : G  C  \n"
+				+ "        D : F  I  B  C  \n"
+				+ "        C : I  A  E  D  \n"
+				+ "    }\n"
+				+ "}\n";
+		
+		assertEquals(expected, sb.toString());
+	}
 }
 
